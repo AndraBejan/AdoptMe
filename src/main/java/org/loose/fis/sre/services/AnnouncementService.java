2a8294130451ec70454e23cc9175f0f1ac05fb5c
AndraBejan <81906692+AndraBejan@users.noreply.github.com>
2022-05-22 16:50:48 +0300

    Add files via upload

---
+++ src/main/java/org/loose/fis/sre/services/AnnouncementService.java
@@ -0,0 +1,55 @@
+package org.loose.fis.sre.services;
+
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.ObjectRepository;
+import org.loose.fis.sre.exceptions.InvalidPassword;
+import org.loose.fis.sre.exceptions.NameAlreadyExistsException;
+import org.loose.fis.sre.exceptions.NotComplete;
+import org.loose.fis.sre.model.Announcement;
+import org.loose.fis.sre.model.Pet;
+import org.loose.fis.sre.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static org.loose.fis.sre.services.FileSystemService.getPathToFile;
+
+public class AnnouncementService {
+    private static ObjectRepository<Announcement> announcementRepository;
+
+    public static void initDatabase() {
+        Nitrite database = Nitrite.builder()
+                .filePath(getPathToFile("ann.db").toFile())
+                .openOrCreate("test", "test");
+
+        announcementRepository = database.getRepository(Announcement.class);
+    }
+    public static void addAnnouncement(Pet petName, User user, String category) {
+        announcementRepository.insert(new Announcement(petName, user, category));
+    }
+    public static Announcement getIdAnnouncement(String id){
+        System.out.println("\nSearching for announcement ID " + id);
+        for(Announcement crt : announcementRepository.find()){
+            if(crt.getID().equals(id)){
+                System.out.println("Announcement was found");
+                return crt;
+            }
+        }
+        System.out.println("Announcement with ID " + id + " was not found");
+        return null;
+    }
+
+    public static ArrayList<Announcement> getUserAnnouncements(String username){
+        System.out.println("\nRetrieving announcement arrayList from database for user " + username);
+        ArrayList<Announcement> userAds= new ArrayList<>();
+        for(Announcement crt : announcementRepository.find()){
+            if(Objects.equals(crt.getUser().getName(),username)){
+                userAds.add(crt);
+            }
+        }
+        System.out.println("Operation was completed successfully");
+        return userAds;
+    }
+
+}
