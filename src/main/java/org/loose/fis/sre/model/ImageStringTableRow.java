471e8cb80ebea882d2a03a1cd104fcdd3f1a06af
AndraBejan <81906692+AndraBejan@users.noreply.github.com>
2022-05-22 16:50:23 +0300

    Add files via upload

---
+++ src/main/java/org/loose/fis/sre/model/ImageStringTableRow.java
@@ -0,0 +1,31 @@
+package org.loose.fis.sre.model;
+
+import javafx.scene.image.ImageView;
+
+public class ImageStringTableRow {
+    private ImageView imageView;
+    private final String info;
+    private final String ID;
+
+    public ImageStringTableRow(ImageView imageView, String info, String ID){
+        this.imageView = imageView;
+        this.info = info;
+        this.ID = ID;
+    }
+
+    public final String getID(){
+        return ID;
+    }
+
+    public final String getInfo(){
+        return info;
+    }
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    public void setImageView(ImageView imageView){
+        this.imageView = imageView;
+    }
+}
